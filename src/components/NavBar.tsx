ab5247940e344c44387affb3acfa162918e165be
John McNeill <jh_mcneill@yahoo.ca>
2021-04-21 20:29:38 -0400

    first commit

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Box, Link, Flex, Button, Heading } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { useMeQuery, useLogoutMutation } from "../generated/graphql";
+import { isServer } from "../utils/isServer";
+import { useRouter } from "next/router";
+import { useApolloClient } from "@apollo/client";
+
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const router = useRouter();
+  const [logout, { loading: logoutFetching }] = useLogoutMutation();
+  const apolloClient = useApolloClient();
+  const { data, loading } = useMeQuery({
+    skip: isServer(),
+  });
+
+  let body = null;
+
+  // data is loading
+  if (loading) {
+    // user not logged in
+  } else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link mr={2}>login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link>register</Link>
+        </NextLink>
+      </>
+    );
+    // user is logged in
+  } else {
+    body = (
+      <Flex align="center">
+        <Box mr={2}>{data.me.email}</Box>
+        <Button
+          onClick={async () => {
+            await logout();
+            await apolloClient.resetStore();
+          }}
+          isLoading={logoutFetching}
+          variant="link"
+        >
+          logout
+        </Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex zIndex={1} position="sticky" top={0} bg="tan" p={4}>
+      <Flex flex={1} m="auto" align="center" maxW={800}>
+        <NextLink href="/">
+          <Link>
+            <Heading>LiReddit</Heading>
+          </Link>
+        </NextLink>
+        <Box ml={"auto"}>{body}</Box>
+      </Flex>
+    </Flex>
+  );
+};
---
+++ src/pages/login.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import { Box, Button, Link, Flex } from "@chakra-ui/react";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/formComponents/InputField";
+import { useLoginMutation, MeQuery, MeDocument } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
+import NextLink from "next/link";
+import { withApollo } from "../utils/withApollo";
+
+export const Login: React.FC<{}> = ({}) => {
+  const router = useRouter();
+  const [login] = useLoginMutation();
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await login({
+            variables: values,
+            update: (cache, { data }) => {
+              cache.writeQuery<MeQuery>({
+                query: MeDocument,
+                data: {
+                  __typename: "Query",
+                  me: data?.login.user,
+                },
+              });
+              cache.evict({ fieldName: "posts:{}" });
+            },
+          });
+          if (response.data?.login.errors) {
+            setErrors(toErrorMap(response.data.login.errors));
+          } else if (response.data?.login.user) {
+            if (typeof router.query.next === "string") {
+              router.push(router.query.next);
+            } else {
+              // worked
+              router.push("/");
+            }
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField name="email" placeholder="email" label="email" />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+            <Flex mt={2}>
+              <NextLink href="/forgot-password">
+                <Link ml="auto">forgot password?</Link>
+              </NextLink>
+            </Flex>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              colorScheme="teal"
+            >
+              login
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default withApollo({ ssr: false })(Login);
